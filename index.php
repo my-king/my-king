29e8c8faf624565cbb2d9bf660f7b3364ee59b26
Igor Santos <igordahora@gmail.com>
2013-06-28 15:44:44 -0300

    Pagina Inicial

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+$config = parse_ini_file("system/config/config.ini", true);
+
+foreach ($config["includes"] as $file) {
+    require_once "system/".$file.'.php';
+}
+
+// Definir as constante do sistema
+Path::getInstancia();
+
+// iniicia aplicação
+$start = new System();
+$start->run();
+
+?>
